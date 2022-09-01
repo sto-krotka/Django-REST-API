7b41919646ba6352a826a02f5b8e097ffb8f4d19
Ania Nieżychowska <aniamaciejewska320@gmail.com>
2022-09-01 16:41:44 +0200

    rest api

---
+++ project/showtimes/serializers.py
@@ -0,0 +1,26 @@
+from showtimes.models import Cinema, Screening
+from rest_framework import serializers
+from movielist.models import Movie
+
+
+class CinemaSerializer(serializers.ModelSerializer):
+    movies = serializers.HyperlinkedIdentityField(
+        many=True,
+        read_only=True,
+        view_name='movies-detail'
+    )
+
+    class Meta:
+        model = Cinema
+        fields = ['name', 'city', 'movies']
+
+
+class ScreeningSerializer(serializers.ModelSerializer):
+    cinema = serializers.SlugRelatedField(slug_field='name',
+                                          queryset=Cinema.objects.all())
+    movie = serializers.SlugRelatedField(slug_field='title',
+                                         queryset=Movie.objects.all())
+
+    class Meta:
+        model = Screening
+        fields = ['movie', 'cinema', 'date']
---
+++ project/showtimes/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render
+from rest_framework import generics
+
+from showtimes.models import Cinema, Screening
+from showtimes.serializers import CinemaSerializer, ScreeningSerializer
+
+
+class CinemaListView(generics.ListCreateAPIView):
+    queryset = Cinema.objects.all()
+    serializer_class = CinemaSerializer
+
+
+class CinemaView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Cinema.objects.all()
+    serializer_class = CinemaSerializer
+
+
+class ScreeningListView(generics.ListCreateAPIView):
+    queryset = Screening.objects.all()
+    serializer_class = ScreeningSerializer
+
+
+class SceeningView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Screening.objects.all()
+    serializer_class = ScreeningSerializer
